d02eb9d434d1df73f61e2cbe73edd8e3b0d254f8
Fawers <fabricioswerneck@gmail.com>
2015-02-05 14:01:06 -0200

    updated ilist

--- ilist.py
+++ ilist.py
@@ -17,48 +17,40 @@ class ilist(object):
         return self._tail
 
     def map(self, function):
-        if self:
-            return function(self.head) + self.tail.map(function)
-        else:
-            return self
+        return function(self.head) + self.tail.map(function)
 
     def filter(self, predicate):
-        if self:
-            if predicate(self.head):
-                return self.head + self.tail.filter(predicate)
-            else:
-                return self.tail.filter(predicate)
+        if predicate(self.head):
+            return self.head + self.tail.filter(predicate)
         else:
-            return self
+            return self.tail.filter(predicate)
 
     def take(self, howmany):
-        if not self or howmany == 0:
+        if howmany == 0:
             return nil()
         else:
             return self.head + self.tail.take(howmany-1)
 
     def take_while(self, predicate):
-        if not self or not predicate(self.head):
+        if not predicate(self.head):
             return nil()
         else:
             return self.head + self.tail.take_while(predicate)
 
     def drop(self, howmany):
-        if not self or howmany == 0:
+        if howmany == 0:
             return self
         else:
             return self.tail.drop(howmany-1)
 
     def drop_while(self, predicate):
-        if not self or not predicate(self.head):
+        if not predicate(self.head):
             return self
         else:
             return self.tail.drop_while(predicate)
 
     def reduce(self, function, acc=None):
-        if not self:
-            return acc
-        elif acc is None:
+        if acc is None:
             return self.tail.reduce(function, self.head)
         else:
             return self.tail.reduce(function, function(self.head, acc))
@@ -77,10 +69,6 @@ class ilist(object):
         else:
             return a_list[0] + ilist.from_list(a_list[1:])
 
-    @staticmethod
-    def from_values(*values):
-        return ilist.from_list(values)
-
     def __repr__(self):
         return repr(self._head)+':'+repr(self._tail)
 
@@ -93,15 +81,19 @@ class ilist(object):
 
     # append & concatenate
     def __add__(self, other):
-        # append
-        if not isinstance(other, ilist):
-            return self.head + (self.tail + other)
-        # concatenate
-        else:
-            if not self.tail: # tail is nil
-                return self.head + other
-            else:
-                return self.head + (self.tail + other)
+        return self.head + (self.tail + other)
+
+    def __getitem__(self, index):
+        l = len(self)
+        while index < 0:
+            index += l
+        return self.drop(index).head
+
+    @property
+    def length(self):
+        return len(self)
+    def __len__(self):
+        return 1 + len(self.tail)
 
 class nil(ilist):
     __metaclass__ = singleton
@@ -119,6 +111,40 @@ class nil(ilist):
     def head(self):
         raise ValueError("head of nil")
 
+    def map(self, function):
+        return self
+
+    def filter(self, predicate):
+        return self
+
+    def take(self, howmany):
+        return self
+    
+    def take_while(self, predicate):
+        return self
+    
+    def drop(self, howmany):
+        return self
+    
+    def drop_while(self, predicate):
+        return self
+    
+    def reduce(self, function, acc):
+        return acc
+
     def __add__(self, other):
         if not isinstance(other, ilist):
-            return other + self
+            return ilist(other)
+        else:
+            return other
+
+    def __eq__(self, other):
+        return other is nil()
+
+    def __getitem__(self, index):
+        raise ValueError("nil[{0}] <=> [][{0}]".format(index))
+
+    def __len__(self):
+        return 0
+
+a = ilist.from_list(range(10))
